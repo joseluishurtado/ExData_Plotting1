068fa98b163f8e07fa13b062f466e516801953af
Jose Hurtado <jh1261011@gmail.com>
2018-10-01 14:46:04 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -42,7 +42,7 @@ par(mfcol = c(1,1))
 hist(x1$Global_active_power,col="red", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", main = "Global Active Power")
 
 ## Copy to PNG File
-dev.copy(png, file = "plot1.png")
+dev.copy(png, file = "plot1.png", width = 480, height = 480)
 dev.off()
 
 
